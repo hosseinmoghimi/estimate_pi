0fbc6e2b434e1df4864fde08020fb097f70c6a82
hossein.moghimi.ce <hossein.moghimi.ce@gmail.com>
2022-02-20 15:18:32 +0330

    code completed

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+from math import sqrt
+from random import random
 import numpy as np
+
+
+n_iter=100
+n_points=1000
+ 
 
 class Point():
     """
@@ -7,6 +14,8 @@ class Point():
         x: float, x-coordinate
         y: float, y-coordinate
     """
+    x=0.0
+    y=0.0
 
     def __init__(self, xy=None):
         """
@@ -14,15 +23,18 @@ class Point():
         If xy is not provided, randomly generate xy coordiantes.
         Otherwise, xy can be assigned by a tuple (e.g., (2, 3))
         """
+        x_random=random()*2
+        y_random=random()*2
         # properties
         self.x = None
         self.y = None
 
         # assign values to x and y
         if xy is None:
-            pass
+            self.x=x_random
+            self.y=y_random
         else:
-            pass
+            (self.x,self.y)=xy
 
     def __repr__(self):
         """
@@ -35,19 +47,36 @@ class Point():
         input: a Point() object
         output: Euclidean distance between self point and the input point
         """
-        pass
+        return sqrt((self.x-point.x)*(self.x-point.x)+(self.y-point.y)*(self.y-point.y))
+        
+        
 
-def estimate_pi(n_points):
+def estimate_pi(n_points=n_points):
     """
     input: number of points to be generated
     output: estimated pi through the simulation
     """
-    pass
+    n_red=0
+    n_black=0
+    for i in range(n_points):
+        point=Point()
+        distance=point.distance(center)
+        if distance<=1:
+            n_red+=1
+        if distance>1:
+            n_black+=1
+    pi=4*n_red/(n_red+n_black)
+    return pi
 
 
 # estimate pi for 100 iterations
-n_iter = 100
+
+center=Point(xy=(1,1))
 pi = np.zeros(n_iter)
+pis=0
 for i in range(n_iter):
-    pass
+    pi[i]=estimate_pi(n_points)
+
+
+print(f"estimated_pi : {np.average(pi)}")
 
